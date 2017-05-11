bad55ccd748e1925d0e0ba38fee3265ca21c56db
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-11 20:34:53 +0000

    fixed delete and add new url bug

--- express_server.js
+++ express_server.js
@@ -1,16 +1,21 @@
 // "use strict";
 
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
-app.use(cookieParser());
 const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+app.use(function(req, res, next){
+  res.locals.username = req.cookies.username;
+  next();
+});
+
+var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -29,9 +34,6 @@ function generateRandomString() {
 
 
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 app.get("/urls", (req, res) => {
   const username = req.cookies["username"];
   let templateconsts = {
@@ -40,16 +42,14 @@ app.get("/urls", (req, res) => {
 };
   res.render("urls_index", templateconsts);
 });
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  console.log(req.body);
-  console.log(shortURL);
   urlDatabase[shortURL] = req.body.longURL;
-
   res.redirect('/urls/' + shortURL);
 });
 app.get("/urls/:id", (req, res) => {
@@ -64,7 +64,8 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
+  let keyId = req.params.id;
+  delete urlDatabase[keyId];
   res.redirect('/urls');
 });
 app.post('/urls/:id/', (req, res) => {
